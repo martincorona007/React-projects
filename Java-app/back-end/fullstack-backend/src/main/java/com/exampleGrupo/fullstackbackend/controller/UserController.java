372e0149f4aada6c23d8547edbed149eb8d1384e
martincorona007 <martincorona007@hotmail.com>
2022-10-10 17:27:36 -0500

    Java-app backend frontend Post data into database AXIOS POST

--- Java-app/back-end/fullstack-backend/src/main/java/com/exampleGrupo/fullstackbackend/controller/UserController.java
+++ Java-app/back-end/fullstack-backend/src/main/java/com/exampleGrupo/fullstackbackend/controller/UserController.java
@@ -3,14 +3,12 @@ package com.exampleGrupo.fullstackbackend.controller;
 import com.exampleGrupo.fullstackbackend.model.User;
 import com.exampleGrupo.fullstackbackend.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
+@CrossOrigin("http://localhost:3000")
 public class UserController {
     //inject the user repository interface
     @Autowired
--- Java-app/front-end/src/App.js
+++ Java-app/front-end/src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
 
+
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css"
+import Navbar from './layout/Navbar';
+import Home from "./pages/Home";
+import {BrowserRouter as Router, Routes,Route} from "react-router-dom";
+import AddUser from "./users/AddUser";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="">
+      <Router>
+        <Navbar/>
+        <Routes>
+          <Route exact path="/" element={<Home/>}/>
+          <Route exact path="/adduser" element={<AddUser/>}/>
+        </Routes>
+      </Router>
+    
     </div>
   );
 }
