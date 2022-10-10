edc8e2159233013ab8afdcf5aa40b348f82e7164
martincorona007 <martincorona007@hotmail.com>
2022-10-10 10:44:10 -0500

    Java-app PostMapping for sending data in database

---
+++ Java-app/back-end/fullstack-backend/src/main/java/com/exampleGrupo/fullstackbackend/controller/UserController.java
@@ -0,0 +1,19 @@
+package com.exampleGrupo.fullstackbackend.controller;
+
+import com.exampleGrupo.fullstackbackend.model.User;
+import com.exampleGrupo.fullstackbackend.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+    //inject the user repository interface
+    @Autowired
+    private UserRepository userRepository;
+    @PostMapping("/user")
+    User newUser(@RequestBody User newUser){
+        return userRepository.save(newUser);
+    }
+}
