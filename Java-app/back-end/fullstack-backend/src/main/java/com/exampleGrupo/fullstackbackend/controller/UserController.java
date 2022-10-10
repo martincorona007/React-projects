45326bb0db8c817868da619073afbebea6e9f55e
martincorona007 <martincorona007@hotmail.com>
2022-10-10 11:06:15 -0500

    Java-app GetMapping for getting data from database

--- Java-app/back-end/fullstack-backend/src/main/java/com/exampleGrupo/fullstackbackend/controller/UserController.java
+++ Java-app/back-end/fullstack-backend/src/main/java/com/exampleGrupo/fullstackbackend/controller/UserController.java
@@ -3,9 +3,12 @@ package com.exampleGrupo.fullstackbackend.controller;
 import com.exampleGrupo.fullstackbackend.model.User;
 import com.exampleGrupo.fullstackbackend.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class UserController {
@@ -15,5 +18,9 @@ public class UserController {
     @PostMapping("/user")
     User newUser(@RequestBody User newUser){
         return userRepository.save(newUser);
+    }
+    @GetMapping("/users")
+    List<User> getAllUsers(){
+        return userRepository.findAll();
     }
 }
